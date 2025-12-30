7e8c298a8870a2ee83411a601c0d80a6126b1b1d
Cesar Aguilar <Blueavian9@gmail.com>
2025-12-30 14:38:03 -0800

    updated 0002_booking_schema.sql

---
+++ migrations/0002_booking_schema.sql
@@ -0,0 +1,9 @@
+-- 0002_booking_core.sql
+PRAGMA foreign_keys = ON;
+
+ALTER TABLE practitioners ADD COLUMN email TEXT;
+ALTER TABLE practitioners ADD COLUMN is_active INTEGER NOT NULL DEFAULT 1;
+
+CREATE UNIQUE INDEX IF NOT EXISTS idx_practitioners_email ON practitioners(email);
+
+ALTER TABLE audit_logs ADD COLUMN metadata_json TEXT NOT NULL DEFAULT '{}';
