28d89b22c5ce857e77affca2b7e011a0e50a8924
Cesar Aguilar <Blueavian9@gmail.com>
2025-12-30 14:48:12 -0800

    updated 0001_initial.sql

--- migrations/0001_initial.sql
+++ migrations/0001_initial.sql
@@ -1,9 +1,7 @@
--- 0002_booking_core.sql
-PRAGMA foreign_keys = ON;
+CREATE TABLE IF NOT EXISTS messages (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  content TEXT NOT NULL DEFAULT 'Hello World from D1!'
+);
 
-ALTER TABLE practitioners ADD COLUMN email TEXT;
-ALTER TABLE practitioners ADD COLUMN is_active INTEGER NOT NULL DEFAULT 1;
+INSERT OR IGNORE INTO messages (id, content) VALUES (1, 'Hello World from D1!');
 
-CREATE UNIQUE INDEX IF NOT EXISTS idx_practitioners_email ON practitioners(email);
-
-ALTER TABLE audit_logs ADD COLUMN metadata_json TEXT NOT NULL DEFAULT '{}';
